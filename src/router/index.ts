e3c88d0bb1e2a37b421e0480a1114d2008a135fd
noah lubko <noah.lubko@ness-tech.co.il>
2019-12-09 10:54:38 +0200

    first commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,21 +2,37 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
+import Test1Layout from "../components/Layouts/Test1Layout.vue";
+import Test2Layout from "../components/Layouts/Test2Layout.vue";
+import Test1 from "../components/Test1.vue";
+import Test2 from "../components/Test2.vue";
+
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: Home
+    path: '',
+    name: 'Test1Layout',
+    component: Test1Layout,
+    children:[
+      {
+        path:'Test1',
+        name:'Test1',
+        component:Test1,
+      }
+    ]
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '',
+    name: 'Test2Layout',
+    component: Test2Layout,
+    children:[
+      {
+        path:'Test2',
+        name:'Test2',
+        component:Test2,
+      }
+    ]
   }
 ]
 
